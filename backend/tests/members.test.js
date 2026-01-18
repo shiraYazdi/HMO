a044cb3e5ce8a7f53971a6dd8027e59d61aa596b
ShiraYazdi <shira6461@gmail.com>
2026-01-18 23:32:10 +0000

    Fix: resolve syntax error in members.test.js

--- backend/tests/members.test.js
+++ backend/tests/members.test.js
@@ -1,5 +1,5 @@
-const request = require('supertest');
-const app = require('../app')
+import app from "../app"
+import request from "supertest"
 
 describe('Members API', () => {
   it('should get all members', async () => {
